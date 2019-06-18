a2ea3d043fa35d88a762211f6ef88b777303389b
Frank Li <b439988l@gmail.com>
2019-06-18 22:51:05 +0800

    incomplete routing and hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import '../css/App.css';
 import Card from 'react-bootstrap/Card';
-import {AppForm} from './Form'
+import { AppForm } from './Form';
+import { Result } from './Result'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 function App() {
+  const [render, setRender] = useState('form');
+  const [loading, setLoading] = useState(false);
   
+
   return (
-    <div className="App">
-      <Card>
-        <Card.Header>Three Point Six</Card.Header>
-        <AppForm />
-        <p>A little experimental MERN stack app by Frank Li and Sebastian Muir-Smith @ QUT, Brisbane</p>
-      </Card>
-    </div>
+    <Router>
+      <div className="App">
+        <Card>
+          <Card.Header>Three Point Six</Card.Header>
+          <Route path="/" exact component={AppForm} />
+          <Route path="/results" component={Result} />
+          <p>A little experimental MERN stack app by Frank Li and Sebastian Muir-Smith @ QUT, Brisbane</p>
+        </Card>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Result.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+
+export function Result(props){
+    return(
+        <Card.Body>
+            
+        </Card.Body>
+    )
+}
