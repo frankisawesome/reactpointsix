a57a95510c1710d6dbec435f0db915d5207370a6
Frank Li <b439988l@gmail.com>
2019-06-20 14:18:52 +0800

    everything works

--- src/Api.js
+++ src/Api.js
@@ -1,19 +1,20 @@
 const url = 'https://threepointsix.azurewebsites.net/api/post';
 
-export function PostNew(message){
+export function PostNew(message) {
+    const body = JSON.stringify({
+        "Message": message,
+        "Authors": ["thatoneguy"]
+    })
     return fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: {
-            "Message": message,
-            "Authors": ["thatoneguy"]
-        }
+        body: body
     })
 }
 
-export function GetMessages(){
+export function GetMessages() {
     return fetch(url, {
         method: 'GET',
     })
